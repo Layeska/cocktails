db6a0265debf1b0e6e497643c6f63d6b30c01b0a
Ana Juárez <lisa13suarez@gmail.com>
2023-06-12 13:24:57 -0600

    ✒️Editing App

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ListComponent } from './pages/list/list.component';
+import { DetailComponent } from './pages/detail/detail.component';
+
+const routes: Routes = [
+  { path: '', component: ListComponent },
+  { path: 'cocktail/:id', component: DetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetailComponent } from './pages/detail/detail.component';
+import { ListComponent } from './pages/list/list.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ItemComponent } from './components/item/item.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { FooterComponent } from './components/footer/footer.component';
+import { PopularComponent } from './components/popular/popular.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailComponent,
+    ListComponent,
+    FooterComponent,
+    PopularComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    HeaderComponent,
+    ItemComponent
   ],
-  providers: [],
+  providers: [provideAnimations()],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
