42da9bfbd00136c8943976f6c34c8b8b8751d43e
Ana Juárez <lisa13suarez@gmail.com>
2023-06-13 12:48:20 -0600

    ✒️Change to standalone

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,24 +1,29 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { NgIf } from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
 import { Cocktail } from 'src/app/interfaces/item.interface';
 import { ItemService } from 'src/app/services/item.service';
 import { Observable, tap } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
-  styleUrls: ['./detail.component.scss']
+  styleUrls: ['./detail.component.scss'],
+  standalone: true,
+  imports: [MatCardModule, NgIf, CommonModule]
 })
 
 export class DetailComponent {
   id!: string
   card$!: Observable<Cocktail>
+  items: Cocktail[] = []
 
   constructor(private route: ActivatedRoute, private itemService: ItemService) {}
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id') || ''
-    console.log('Hola ', this.id)
-    this.card$ = this.itemService.getById(this.id).pipe(tap(console.log))
+    this.card$ = this.itemService.showDetails(this.id).pipe(tap(console.log))
   }
 }
