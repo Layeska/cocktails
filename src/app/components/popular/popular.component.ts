b5e630607fa0f1e37a5623a53f6e2cac0b5471f0
Ana Juárez <lisa13suarez@gmail.com>
2023-06-12 21:48:15 -0600

    ➕Adding items in array

--- src/app/components/popular/popular.component.ts
+++ src/app/components/popular/popular.component.ts
@@ -10,18 +10,27 @@ import { ItemService } from 'src/app/services/item.service';
 
 export class PopularComponent {
   drink1: Cocktail[] = []
+  item: Cocktail[] = []
 
   constructor(private itemService: ItemService) {}
 
   ngOnInit(): void {
-    this.searchById(11007)
+    this.searchById(11000)
+    this.searchById(12322)
+    this.searchById(17207)
+    this.searchById(15328)
+    this.searchById(178348)
+    this.searchById(178352)
+    this.searchById(11872)
+    this.searchById(14372)
+    //console.log("AUX: ", this.item)
   }
 
   searchById(id: number | string) {
     this.itemService.getById(id).subscribe(res => {
-      console.log("By Id: ", res.drinks[0])
-      this.drink1 = res.drinks[0]
-      console.log(this.drink1)
+      this.drink1 = res.drinks
+     //console.log("drink1: ",this.drink1)
+      this.item.push(this.drink1[0])
     })
   }
 }
