ecceecd9bfa2f0d82c981080453c64bdf7e522d1
Ana Juárez <lisa13suarez@gmail.com>
2023-06-13 21:20:27 -0600

    ➕ Adding material expansion

--- src/app/pages/faq/faq.component.ts
+++ src/app/pages/faq/faq.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
-  styleUrls: ['./faq.component.scss']
+  styleUrls: ['./faq.component.scss'],
+  standalone: true,
+  imports: [MatExpansionModule]
 })
 export class FaqComponent {
-
+  panelOpenState = false;
 }
